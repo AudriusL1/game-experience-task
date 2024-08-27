2357fcda3ac619dde20f90c53b6634f979c1b288
AudriusL1 <alukosevicius18@gmail.com>
2024-08-27 17:28:04 +0300

    created notification job for future

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\DailyFeedbackInfoNotificationJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        //TODO: uncomment the following lines to enable the job when it is implemented
+//        $schedule->job(new DailyFeedbackInfoNotificationJob())->dailyAt('05:00');
     }
 
     /**
@@ -20,7 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
